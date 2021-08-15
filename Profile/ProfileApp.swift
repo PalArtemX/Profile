310022f67205bb5bc91d9fbb1e7c6bd5ff66029e
Artem Palyutin <pal.artem@icloud.com>
2021-08-15 12:32:41 +0300

    Initial Commit

---
+++ Profile/ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileApp.swift
+//  Profile
+//
+//  Created by Artem Palyutin on 15.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
